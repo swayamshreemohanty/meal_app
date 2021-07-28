b053b300a66feb0bc2fe7be085a09eb45ba71b04
swayamshreemohanty <mohantyswayamshree@gmail.com>
2021-07-28 20:32:48 +0530

    dev1 V0.0

---
+++ lib/category_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen(id, title);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color
+            ], //the first "color.withOpacity(0.7)" is the starting point and the "color" is the ending point.
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Text('The recipes for the catagory!'),
+      ),
+    );
+  }
+}
